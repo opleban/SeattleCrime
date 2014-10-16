1359e392c510a70f96f7b01cc9692e62b47d5024
Ori <opleban@gmail.com>
2014-10-16 12:16:04 -0700

    creates constant for mile in meters

--- app/models/SODAQuery.rb
+++ app/models/SODAQuery.rb
@@ -6,6 +6,7 @@ class SODAQuery
   end
 
   def get_CenturyLink_data
-    response = seattle_crime_client.get("3k2p-39jp", { "$where" => "within_circle(incident_location, 47.595941, -122.331515, 1610)"})
+    MILE_IN_METERS = 1610
+    response = seattle_crime_client.get("3k2p-39jp", { "$where" => "within_circle(incident_location, 47.595941, -122.331515, #{MILE_IN_METERS)}"})
   end
 end
